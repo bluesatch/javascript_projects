a8c4aefaab7b83a1ea59ae046d8f53e38b486c9b
Satchel Page <hebrown12@gmail.com>
2018-10-12 15:51:19 -0500

    made changes to calculator

--- chapter2/js/invcalc.js
+++ chapter2/js/invcalc.js
@@ -1,9 +1,23 @@
 function investmentcalc() {
-    var investment = document.getElementById('investment').value;
-    var intRate = document.getElementById('intRate').value;
+    event.preventDefault();
+    var investment = Number(document.getElementById('investment').value);
+    var intRate = Number(document.getElementById('intRate').value);
+    var periods = Number(document.getElementById('periods').value);
+    var additionalAmount = Number(document.getElementById('additionalAmount').value);
+    
     var term = document.getElementById('term').value;
-    var additionalAmount = document.getElementById('additionalAmount').value;
+
+    if (term == "year") {
+        var intEarned = (investment * intRate) * periods;
+    }; 
+
+    if (term == "month") {
+        var intEarned = (investment * intRate) * (periods / 12);
+    };
+    var output = document.getElementById('totalAmount');
+    var outputIntEarned = document.getElementById('intEarned');
+    var totalAmount = intEarned + investment + additionalAmount;
+    outputIntEarned.innerHTML = "$" + intEarned.toFixed(2);
+    output.innerHTML = "$" + totalAmount.toFixed(2);
 }
 
-intEarned.innerHTML = intEarned;
-totalAmount.innerHTML = totalAmount;
